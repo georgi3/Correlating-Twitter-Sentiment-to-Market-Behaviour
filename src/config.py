f38a8e2e5c2addad83ecadf5cd9f1edd39f5beda
George Marchenko <george.marchenko44@gmail.com>
2022-06-10 07:41:21 -0400

    init

---
+++ src/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.environ['API_KEY']
+API_SECRET_KEY = os.environ['SECRET_API']
+BEARER_TOKEN = os.environ['BEARER_TOKEN']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
