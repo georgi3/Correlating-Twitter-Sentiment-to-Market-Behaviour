a58ce7a7b90df69ad3ea97ae62cfe0c539bf06ed
George Marchenko <george.marchenko44@gmail.com>
2022-06-11 19:30:07 -0400

    added more configurations

--- src/config.py
+++ src/config.py
@@ -1,9 +1,22 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
+from os.path import join, dirname
 
-API_KEY = os.environ['API_KEY']
-API_SECRET_KEY = os.environ['SECRET_API']
-BEARER_TOKEN = os.environ['BEARER_TOKEN']
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
-ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+dotenv_path = join(dirname(__file__), '.env')
+
+load_dotenv(dotenv_path)
+
+GC_API_KEY = os.environ['GC_API_KEY']
+GC_API_SECRET_KEY = os.environ['GC_SECRET_API']
+GC_BEARER_TOKEN = os.environ['GC_BEARER_TOKEN']
+GC_ACCESS_TOKEN = os.environ['GC_ACCESS_TOKEN']
+GC_ACCESS_TOKEN_SECRET = os.environ['GC_ACCESS_TOKEN_SECRET']
+
+API_KEY = os.environ['API_KEY_PR']
+API_SECRET_KEY = os.environ['SECRET_API_PR']
+BEARER_TOKEN = os.environ['BEARER_TOKEN_PR']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN_PR']
+ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET_PR']
+
+TARGET_ACCOUNTS_1 = ['BitcoinMagazine', 'CoinDesk']
+TARGET_ACCOUNTS_2 = ['Bitcoin', 'BT'
