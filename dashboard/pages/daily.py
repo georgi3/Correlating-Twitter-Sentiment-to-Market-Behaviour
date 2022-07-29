cc004db509ba82b0310edfadfb73c73d7bdcca0a
George Marchenko <george.marchenko44@gmail.com>
2022-07-29 19:22:14 +0200

    added loading images, started styling

--- dashboard/pages/daily.py
+++ dashboard/pages/daily.py
@@ -1,31 +1,37 @@
 import pandas as pd
 import json
 import dash
-from dash import html, dcc, Output, Input, callback, dash_table
-
+import time
+from dash import html, dcc, Output, Input, callback
+import dash_bootstrap_components as dbc
 from data_managing.db_handler import retrieve_data
 from src.archive.data_managing.preprocessing import daily_pipe
 from dashboard.other.supporting_scripts import query_df, get_dataframe, parse_input, plot_sentiment_btc_timeseries, \
-    plot_volume_tweet_count_timeseries, plot_correlation_heatmap, plot_pie_chart, create_dash_table
-from dashboard.other.settings import ACCOUNTS, TS_DROPDOWN_OPTIONS_X, TS_DROPDOWN_OPTIONS_Y, HM_DROPDOWN_OPTIONS,\
+    plot_volume_tweet_count_timeseries, plot_correlation_heatmap, plot_pie_chart, create_dash_table, navbar
+from dashboard.other.settings import ACCOUNTS, TS_DROPDOWN_OPTIONS_X, TS_DROPDOWN_OPTIONS_Y, HM_DROPDOWN_OPTIONS, \
     DROPDOWN_SENTIMENT
 
 dash.register_page(__name__, path='/daily-analysis/')
 
 
 @callback(
-    Output('datatable_div', 'children'),
-    Input('corr_with_value', 'data'),
+    Output('corr-table-daily', 'children'),
+    Input('corr-table-storage', 'data'),
 )
 def datatable(session_storage):
     df = pd.read_json(json.loads(session_storage)['dataframe'], orient='split')
     return create_dash_table(df)
 
 
-g@callback(
-    Output('pie-fig', 'figure'),
+# @callback(
+#     Input
+# )
+
+
+@callback(
+    Output('pie-chart-daily', 'figure'),
     Input('datatable', 'selected_rows'),
-    Input('corr_with_value', 'data'),
+    Input('corr-table-storage', 'data'),
 )
 def pie_chart(indices, session_storage):
     dataframe = pd.read_json(json.loads(session_storage)['dataframe'], orient='split')
@@ -34,12 +40,12 @@ def pie_chart(indices, session_storage):
 
 
 @callback(
-    Output('daily-timeseries', 'figure'),
-    Input('ts-dropdown-y', 'value'),
-    Input('ts-dropdown-y', 'options'),
-    Input('ts-dropdown-x', 'value'),
-    Input('ts-dropdown-x', 'options'),
-    Input('rolling-window', 'value'),
+    Output('ts-sentiment-btc-daily', 'figure'),
+    Input('ts-dropdown-y-daily', 'value'),
+    Input('ts-dropdown-y-daily', 'options'),
+    Input('ts-dropdown-x-daily', 'value'),
+    Input('ts-dropdown-x-daily', 'options'),
+    Input('rolling-window-daily', 'value'),
     Input('filtered_data_storage', 'data'),
 )
 def sentiment_btc_timeseries(y_value_1, y_label_1, y_value_2, y_label_2, window, filtered_data):
@@ -49,9 +55,9 @@ def sentiment_btc_timeseries(y_value_1, y_label_1, y_value_2, y_label_2, window,
 
 
 @callback(
-    Output('daily-volume', 'figure'),
+    Output('ts-volume-daily', 'figure'),
     Input('datatable', 'selected_rows'),
-    Input('corr_with_value', 'data'),
+    Input('corr-table-storage', 'data'),
     Input('filtered_data_storage', 'data'),
 )
 def volume_vs_tweet_count(indices, session_storage, filtered_data):
@@ -63,7 +69,7 @@ def volume_vs_tweet_count(indices, session_storage, filtered_data):
 
 
 @callback(
-    Output('daily-heatmap', 'figure'),
+    Output('heatmap-daily', 'figure'),
     Input('hm-dropdown', 'value'),
     Input('filtered_data_storage', 'data'),
 )
@@ -72,9 +78,11 @@ def heatmap(values, filtered_data):
     return plot_correlation_heatmap(data, values)
 
 
+# TODO Data Storing Related Functions
 @callback(
-    Output('corr_with_value', 'data'),
-    Input('sentiment_table_dd', 'value')
+    Output('corr-table-storage', 'data'),
+    Output('corr-table-daily', 'className'),
+    Input('sentiment_table_daily', 'value'),
 )
 def store_corr_with(corr_with):
     dataframe = acc_corr_table(corr_with)
@@ -82,15 +90,19 @@ def store_corr_with(corr_with):
         'corr_with': corr_with,
         'dataframe': dataframe.to_json(date_format='iso', orient='split'),
     }
-    return json.dumps(data)
+    return json.dumps(data), dash.no_update
+
+
+from dashboard.other.serialization import read_serialized
 
 
 @callback(
     Output('filtered_data_storage', 'data'),
     Input('datatable', 'selected_rows'),
-    Input('corr_with_value', 'data')
+    Input('corr-table-storage', 'data')
 )
 def store_df_for_selected_sources(indices, session_storage):
+    data_tweets, btc_daily = read_serialized('data_tweets'), read_serialized('btc_daily')
     dataframe = pd.read_json(json.loads(session_storage)['dataframe'], orient='split')
     accounts_ids, _ = parse_input(dataframe, indices)
     filtered_tweets = query_df(data_tweets, accounts_ids)
@@ -102,7 +114,8 @@ def store_df_for_selected_sources(indices, session_storage):
 
 
 def acc_corr_table(corr_with):
-    global data_tweets, btc_daily
+    # global data_tweets, btc_daily
+    data_tweets, btc_daily = read_serialized('data_tweets'), read_serialized('btc_daily')
     corr_dict = {
         'Account': [],
         'Account_Name': [],
@@ -111,12 +124,14 @@ def acc_corr_table(corr_with):
         'BTC High': [],
         'BTC Low': [],
         'Count': [],
+        'Avg Count': [],
     }
     cols = ['close_price', 'open_price', 'high_price', 'low_price']
     for id_, label in ACCOUNTS.items():
         filtered_tweets = query_df(data_tweets, [id_])
         df = daily_pipe(filtered_tweets, btc_daily)
-        corr = df[cols].corrwith(df[corr_with], ).apply(lambda x: round(x, 3)).to_dict()
+        corr = df[cols].corrwith(df[corr_with], ).apply(lambda x: float(f'{x: .2f}')).to_dict()
+        # corr = corr.apply(lambda x: int(x))
         corr_dict['Account'].append(f'[{label}](https://twitter.com/{label})')
         corr_dict['Account_Name'].append(f'{label}')
         corr_dict['BTC Close'].append(corr['close_price'])
@@ -124,57 +139,46 @@ def acc_corr_table(corr_with):
         corr_dict['BTC High'].append(corr['open_price'])
         corr_dict['BTC Low'].append(corr['low_price'])
         corr_dict['Count'].append(df['tweet_count'].sum())
+        corr_dict['Avg Count'].append(round(df['tweet_count'].sum()/df.__len__(), 2))
     return pd.DataFrame(corr_dict).sort_values(by='BTC Close', ascending=False)
 
-from dashboard.other.serialization import read_serialized
+
 # data_tweets, btc_daily = get_dataframe()
-data_tweets, btc_daily = read_serialized('data_tweets'), read_serialized('btc_daily')
+# data_tweets, btc_daily = read_serialized('data_tweets'), read_serialized('btc_daily')
 
 layout = html.Div([
     html.Div(children=[
         html.Div(
             [
-                html.P("Select:"),
                 dcc.Dropdown(
                     options=DROPDOWN_SENTIMENT,
                     placeholder='Average VADER Compound per Day',
-                    id="sentiment_table_dd",
+                    id="sentiment_table_daily",
                     value='avg_vader_compound_norm',
                     clearable=False,
                 ),
+                dcc.Loading(
+                    id='loading-corr-table-daily',
+                    type='circle',
+                    fullscreen=True,
+                    children=[
+                        html.Div(id='corr-table-daily'),
+                    ]
+                ),
             ],
-            style={'width': '48%', 'display': 'inline-block'}),
-        html.Div(id='datatable_div'),
-        # html.Div(children=[
-        #     dash_table.DataTable(
-        #         id='datatable',
-        #         columns=[
-        #             {'name': i, 'id': i, 'presentation': 'markdown'} if i == 'Account'
-        #             else {'name': i, 'id': i} for i in dataframe.columns
-        #         ],
-        #         data=dataframe.to_dict('records'),
-        #         # filter_action='native',
-        #         sort_action='native',
-        #         sort_mode='multi',
-        #         row_selectable='multi',
-        #         selected_rows=[0],
-        #         # selected_row_ids=[],  # https://github.com/plotly/dash/issues/185 BUG (returns [None, None, ...])
-        #         page_action='native',
-        #         tooltip={i: {'value': i, 'use_with': 'header'} for i in dataframe.columns},
-        #         markdown_options={'html': False},
-        #         # page_current=0,
-        #         # page_count=11,
-        #     ),
-        #     # html.Div(id='data')
-        # ]),
-        dcc.Graph(id='pie-fig'),
+            id='datatable-div',
+            style={'width': '48%', 'display': 'inline-block'},
+        ),
+        dcc.Store(id='corr-table-storage'),
+        # html.Div(id='corr-table-daily'),
+        dcc.Graph(id='pie-chart-daily'),
         html.Div(
             [
                 html.P("Select:"),
                 dcc.Dropdown(
                     options=TS_DROPDOWN_OPTIONS_X,
                     placeholder='Sentiment Analysis',
-                    id="ts-dropdown-x",
+                    id="ts-dropdown-x-daily",
                     value='avg_vader_compound_norm',
                     clearable=False,
                 ),
@@ -185,7 +189,7 @@ layout = html.Div([
             dcc.Dropdown(
                 options=TS_DROPDOWN_OPTIONS_Y,
                 placeholder='Bitcoin Price',
-                id="ts-dropdown-y",
+                id="ts-dropdown-y-daily",
                 value='close_price_norm',
                 clearable=False,
             ),
@@ -194,7 +198,7 @@ layout = html.Div([
         html.Div([
             html.Label('Rolling Mean K'),
             dcc.Input(
-                id='rolling-window',
+                id='rolling-window-daily',
                 type='number',
                 value=3,
                 min=2,
@@ -202,8 +206,8 @@ layout = html.Div([
         ],
             style={'width': '48%', 'display': 'inline-block'},
         ),
-        dcc.Graph(id="daily-timeseries"),
-        dcc.Graph(id='daily-volume'),
+        dcc.Graph(id="ts-sentiment-btc-daily"),
+        dcc.Graph(id='ts-volume-daily'),
         html.Div(children=[
             dcc.Dropdown(
                 id='hm-dropdown',
@@ -212,11 +216,10 @@ layout = html.Div([
                 multi=True
             )
         ]),
-        dcc.Graph(id="daily-heatmap"),
+        dcc.Graph(id="heatmap-daily"),
+        dcc.Store(id='filtered_data_storage')
     ]
     ),
-    dcc.Store(id='corr_with_value'),
-    dcc.Store(id='filtered_data_storage')
 ],
     style={'display': 'flex', 'flex-direction': 'row'},
 )
