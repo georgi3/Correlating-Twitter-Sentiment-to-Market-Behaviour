4c353ad3a1b66419ca384a13a8a1c048597b970b
George Marchenko <george.marchenko44@gmail.com>
2022-08-11 11:00:20 +0200

    added notes

--- dashboard/other/serialization.py
+++ dashboard/other/serialization.py
@@ -2,6 +2,7 @@ import pickle
 
 
 def serialize(**kwargs):
+    # TODO mkdir serialized if not exists
     for label, df in kwargs.items():
         with open(f'serialized/{label}.pickle', 'wb') as f:
             pickle.dump(df, f)
@@ -11,3 +12,6 @@ def read_serialized(label):
     with open(f'serialized/{label}.pickle', 'rb') as f:
         dataframe = pickle.load(f)
     return dataframe
+
+
+
