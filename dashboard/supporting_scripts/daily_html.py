698516647cedfd1630de8202cff373d90da43f45
George Marchenko <george.marchenko44@gmail.com>
2022-08-28 20:51:09 -0400

    added titles to the graphs within html

--- dashboard/supporting_scripts/daily_html.py
+++ dashboard/supporting_scripts/daily_html.py
@@ -6,7 +6,7 @@ import dash_bootstrap_components as dbc
 # Content
 def build_tabs():
     return html.Div(
-        id='tabs-d',
+        id='tabs',
         className='tabs',
         children=[
             dcc.Tabs(
@@ -37,30 +37,75 @@ def build_tabs():
 def render_tab_1():
     return html.Div(
         children=[
-            dcc.Dropdown(
-                options=DROPDOWN_SENTIMENT,
-                placeholder='Average VADER Compound per Day',
-                id="sentiment-table-d-dp",
-                value='avg_vader_compound',
-                clearable=False,
-            ),
-            dcc.Loading(
-                id='loading-corr-table-daily',
-                type='circle',
-                fullscreen=True,
+            html.Div(
+                className='float-container',
                 children=[
-                    html.Div(id='corr-table-div-d'),
+                    html.Div(
+                        className='float-child',
+                        children=[
+                            html.H5(
+                                'Correlation Table for Each Source',
+                                className='title',
+                            ),
+                            html.P('Choose sources for further analysis:'),
+                            dcc.Dropdown(
+                                options=DROPDOWN_SENTIMENT,
+                                placeholder='Average VADER Compound per Day',
+                                id="sentiment-table-d-dp",
+                                value='avg_vader_compound',
+                                clearable=False,
+                            ),
+                            dcc.Loading(
+                                id='loading-corr-table-daily',
+                                type='circle',
+                                fullscreen=True,
+                                children=[
+                                    html.Div(id='corr-table-div-d'),
+                                ]
+                            ),
+                        ]
+                    ),
+                    html.Div(
+                        className='float-child',
+                        children=[
+                            html.H5(
+                                'Distribution of Selected Sources',
+                                className='title',
+                            ),
+                            dcc.Graph(
+                                id='pie-chart-d',
+                                className='pie-chart',
+                                style={'width': '70vh', 'height': '70vh'}
+                            ),
+                        ]
+                    ),
                 ]
             ),
-            dcc.Graph(id='pie-chart-d'),
         ],
-        style={'width': '48%', 'display': 'inline-block'},
     )
 
 
 def render_tab_2():
     return html.Div(
         children=[
+            html.H5(
+                'Timeseries for Sentiment and BTC for Selected Sources',
+                className='title',
+                    ),
+            html.Div(
+                children=[
+                    html.Label('Rolling Mean K'),
+                    dcc.Input(
+                        id='rolling-window-d',
+                        type='number',
+                        value=3,
+                        min=2,
+                        className='rolling-input'
+                    ),
+                ],
+                style={'width': '48%', 'display': 'inline-block'},
+            ),
+            html.Br(),
             html.Div(
                 children=[
                     dcc.Dropdown(
@@ -83,20 +128,16 @@ def render_tab_2():
                 ],
                 style={'width': '48%', 'display': 'inline-block'}
             ),
-            html.Div(
-                children=[
-                    html.Label('Rolling Mean K'),
-                    dcc.Input(
-                        id='rolling-window-d',
-                        type='number',
-                        value=3,
-                        min=2,
-                    ),
-                ],
-                style={'width': '48%', 'display': 'inline-block'},
-            ),
             dcc.Graph(id="ts-sentiment-btc-d"),
+            html.H5(
+                'Timeseries for Normalized BTC Volume and Tweet Count for Selected Sources',
+                className='title',
+            ),
             dcc.Graph(id='ts-volume-d'),
+            html.H5(
+                'Correlation Heatmap for Selected Sources',
+                className='title',
+            ),
             html.Div(
                 children=[
                     dcc.Dropdown(
@@ -118,7 +159,7 @@ def error_modal():
             dbc.ModalHeader(dbc.ModalTitle('Note:'), close_button=False),
             dbc.ModalBody('At least one source has to be selected!'),
             dbc.ModalFooter(
-                dbc.Button('Ok', id='close_error', n_clicks=0)
+                dbc.Button('Ok', id='close_error-d', className='close_error', n_clicks=0)
             ),
         ],
         id='not_selected_sources_error-d',
@@ -134,6 +175,7 @@ def layout():
             error_modal(),
             build_tabs(),
             html.Div(
+                className='tab-content',
                 id='tab-content-d'
             ),
             dcc.Store(id='corr-table-storage-d'),
