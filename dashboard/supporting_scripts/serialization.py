e122409d5171dc08ac9fc19d72bc8894f289a085
George Marchenko <george.marchenko44@gmail.com>
2022-08-28 21:12:59 -0400

    added hourly serialization routine

--- dashboard/supporting_scripts/serialization.py
+++ dashboard/supporting_scripts/serialization.py
@@ -1,3 +1,5 @@
+from dashboard.supporting_scripts.constants import CORR_WITH_VALUES
+from dashboard.supporting_scripts.misc import acc_corr_table, get_dataframes
 import pickle
 import os
 
@@ -27,6 +29,48 @@ def read_serialized(label: str):
     except FileNotFoundError as err:
         raise FileNotFoundError(err)
     return dataframe
+
+
+def serialize_db():
+    """
+    Function that is scheduled to run hourly. It serializes database with tweets abd BTC on hourly basis and does some
+    computations in advance which are also serialized to speed up site speed.
+        Serializes:
+            - data_tweets: pd.DataFrame, (note: very large and constantly growing)
+            - btc_daily: pd.DataFrame, bitcoin daily stats
+            - btc_hourly: pd.DataFrame, bitcoin hourly stats
+            - indices_mapping_d: dict, (note: takes some time to calculate that is why it is serialized hourly)
+                - avg_vader_compound_d: pd.DataFrame, Correlation/Stats Table for every source vs btc stats
+                - avg_tb_subjectivity_d: pd.DataFrame, Correlation/Stats Table for every source vs btc stats
+                - avg_tb_polarity_d: pd.DataFrame, Correlation/Stats Table for every source vs btc stats
+            - indices_mapping_h: dict, (note: takes some time to calculate that is why it is serialized hourly)
+                - avg_vader_compound_h: pd.DataFrame, Correlation/Stats Table for every source vs btc stats
+                - avg_tb_subjectivity_h: pd.DataFrame, Correlation/Stats Table for every source vs btc stats
+                - avg_tb_polarity_h: pd.DataFrame, Correlation/Stats Table for every source vs btc stats
+
+    """
+    data_tweets, btc_daily, btc_hourly = get_dataframes()
+    data_to_serialize = {
+        'data_tweets': data_tweets,
+        'btc_daily': btc_daily,
+        'btc_hourly': btc_hourly,
+    }
+    indices_mapping_d = dict()
+    indices_mapping_h = dict()
+    for value in CORR_WITH_VALUES:
+        corr_table_d = acc_corr_table(value, data_tweets, btc_daily, daily=True)
+        data_to_serialize[f'{value}_d'] = corr_table_d
+        indices_mapping_d[value] = {
+            'names': {name: i for i, name in zip(corr_table_d.index, corr_table_d.Account_Name)}
+        }
+        corr_table_h = acc_corr_table(value, data_tweets, btc_hourly, daily=False)
+        data_to_serialize[f'{value}_h'] = corr_table_h
+        indices_mapping_h[value] = {
+            'names': {name: i for i, name in zip(corr_table_h.index, corr_table_h.Account_Name)}
+        }
+    data_to_serialize['indices_mapping_d'] = indices_mapping_d
+    data_to_serialize['indices_mapping_h'] = indices_mapping_h
+    serialize(**data_to_serialize)
 
 
 
