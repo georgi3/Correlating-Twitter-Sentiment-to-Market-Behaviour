30818e970f52a4f5e4c364c4a2ff7a4dac1bfccd
George Marchenko <george.marchenko44@gmail.com>
2022-09-11 19:21:57 -0400

    deleted appscheduler, crontab will be used instead

--- dashboard/manage_serialization.py
+++ dashboard/manage_serialization.py
@@ -1,10 +1,3 @@
 from supporting_scripts.serialization import serialization_routine
-from apscheduler.schedulers.background import BackgroundScheduler
-import datetime
 
-
-if __name__ == 'manage_serialization':
-    print(f'Running {__name__}...')
-    scheduler = BackgroundScheduler(timezone='US/Eastern')
-    scheduler.add_job(serialization_routine, 'interval', minutes=10, next_run_time=datetime.datetime.now())
-    scheduler.start()
+serialization_routine()
